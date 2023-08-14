dd16d48ad0bbe4a8472a5931d66ce7dc50375461
Paula Biazotto <pds_paula@hotmail.com>
2023-08-14 14:37:02 +0100

    feat: mlx is workinggit add .

--- main.c
+++ main.c
@@ -6,15 +6,21 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 14:11:56 by paula             #+#    #+#             */
-/*   Updated: 2023/08/14 14:14:55 by paula            ###   ########.fr       */
+/*   Updated: 2023/08/14 14:33:49 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+#include "mlx.h"
 
-int main(void)
+int	main(void)
 {
-    ft_printf("hello world");
-    return(0);
+	void	*mlx;
+	void	*mlx_win;
+    (void)mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	mlx_loop(mlx);
 }
 
